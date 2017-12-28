f917b889257917af8b2187de15b1e1d2af0eb40c
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2017-12-29 00:24:18 +0700

    Commit 2

--- hcmiuiot_cpp_vnOCR/hcmiuiot_cpp_vnOCR.cpp
+++ hcmiuiot_cpp_vnOCR/hcmiuiot_cpp_vnOCR.cpp
@@ -1,11 +1,19 @@
-// hcmiuiot_cpp_vnOCR.cpp : Defines the entry point for the console application.
+﻿// hcmiuiot_cpp_vnOCR.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <vector>
 
+using namespace std;
+using namespace cv;
 
 int main()
 {
+
     return 0;
 }
+
 
